83e24cc8958ea264260ba1a860efaac4556fec3c
michiy611 <tech.agili.fkvnpn.611@gmail.comgit config --global user.email tech.agili.fkvnpn.611@gmail.com>
2023-01-03 00:06:54 +0900

    first commit

---
+++ backend/app.py
@@ -0,0 +1,87 @@
+﻿# render_template：参照するテンプレートを指定
+# jsonify：json出力
+from flask import Flask, render_template, jsonify, request, make_response
+
+# CORS：Ajax通信するためのライブラリ
+from flask_restful import Api, Resource
+from flask_cors import CORS 
+from random import *
+from PIL import Image
+from pathlib import Path
+from io import BytesIO
+import base64
+
+# static_folder：vueでビルドした静的ファイルのパスを指定
+# template_folder：vueでビルドしたindex.htmlのパスを指定
+app = Flask(__name__, static_folder = "./../frontend/dist/static", template_folder="./../frontend/dist")
+
+#日本語
+app.config['JSON_AS_ASCII'] = False
+#CORS=Ajaxで安全に通信するための規約
+api = Api(app)
+CORS(app)
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 任意のリクエストを受け取った時、index.htmlを参照
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template("index.html")
+
+# @app.route('/', methods=['POST'])
+@app.route('/classification', methods=['POST'])
+def uploadVideo():
+    if request.method == 'POST':
+        base64_mp4 =  request.form['video']
+        # base64_png =  request.form['image']
+        code = base64.b64decode(base64_mp4.split(',')[1]) 
+        # code = base64.b64decode(base64_png.split(',')[1]) 
+        image_decoded = Image.open(BytesIO(code))
+        image_decoded.save(Path(app.config['UPLOAD_FOLDER']) / 'video.mp4')
+        return make_response(jsonify({'result': 'success'}))
+    else: 
+        return make_response(jsonify({'result': 'invalid method'}), 400)
+
+# 画像版(元コード)
+# def uploadImage():
+#     if request.method == 'POST':
+#         base64_png =  request.form['image']
+#         code = base64.b64decode(base64_png.split(',')[1]) 
+#         image_decoded = Image.open(BytesIO(code))
+#         image_decoded.save(Path(app.config['UPLOAD_FOLDER']) / 'image.png')
+#         return make_response(jsonify({'result': 'success'}))
+#     else: 
+#         return make_response(jsonify({'result': 'invalid method'}), 400)
+
+# app.run(host, port)：hostとportを指定してflaskサーバを起動
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+# render_template：参照するテンプレートを指定
+# jsonify：json出力
+# from flask import Flask, render_template, jsonify
+
+# CORS：Ajax通信するためのライブラリ
+# from flask_cors import CORS
+# from random import *
+
+# # static_folder：vueでビルドした静的ファイルのパスを指定
+# # template_folder：vueでビルドしたindex.htmlのパスを指定
+# app = Flask(__name__, static_folder="../frontend/dist/static", template_folder="../frontend/dist")
+
+# app.config.from_object(__name__)
+
+# CORS(app)
+
+# # 任意のリクエストを受け取った時、index.htmlを参照
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def index(path):
+#     return render_template("index.html")
+
+# # app.run(host, port)：hostとportを指定してflaskサーバを起動
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
---
+++ log.md
@@ -0,0 +1,15 @@
+﻿# アプリの開発録
+
+## Vue + Flask 環境の構築
+
+https://qiita.com/Yoshipy/items/efbe34d9f28ac16f44ad
+エラー解決  
+http://blog.livedoor.jp/tamanooboshi/archives/30603777.html
+
+Vue.js+Flaskで画像のアップロード機能
+https://qiita.com/rockguitar67/items/f8edc33dd221d8f4e965
+
+デザイナーが Vue.js で作る、ローカル動画ファイルのプレビュー画面
+https://blog.vivita.io/entry/2019/04/08/070000
+
+
